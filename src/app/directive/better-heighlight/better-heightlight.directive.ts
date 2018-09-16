5caa5321417be3ece5c1e7b091c8ae549001485d
Siddharth Jadhav <siddharth.jadhav2093@gmail.com>
2018-09-16 16:02:41 +0530

    hostbinding

--- src/app/directive/better-heighlight/better-heightlight.directive.ts
+++ src/app/directive/better-heighlight/better-heightlight.directive.ts
@@ -1,9 +1,12 @@
-import { Directive, ElementRef, Renderer2, OnInit, HostListener } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHeightlight]'
 })
 export class BetterHeightlightDirective implements OnInit{
+
+  // using this you can bind any property on that dom
+  @HostBinding('style.backgroundColor') backroundColor: string = 'transparent';
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
@@ -13,12 +16,14 @@ export class BetterHeightlightDirective implements OnInit{
 
   @HostListener('mouseenter') mouseEnter(eventData: Event) {
     console.log('mouseenter eventData : ', eventData);
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    this.backroundColor = 'red';
   }
 
   @HostListener('mouseleave') mouseLeave(eventDate: Event){
     console.log('mouseleave Event data: ', eventDate);
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    this.backroundColor = 'transparent';
   }
 
 
