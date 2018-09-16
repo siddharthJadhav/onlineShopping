ee2207b8b223327f2aa16d0674c911d84f09b4db
Siddharth Jadhav <siddharth.jadhav2093@gmail.com>
2018-09-16 15:53:56 +0530

    add hostListner on custome directive

--- src/app/directive/better-heighlight/better-heightlight.directive.ts
+++ src/app/directive/better-heighlight/better-heightlight.directive.ts
@@ -1,14 +1,26 @@
-import { Directive, ElementRef, Renderer2, OnInit } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHeightlight]'
 })
-export class BetterHeightlightDirective {
+export class BetterHeightlightDirective implements OnInit{
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseenter') mouseEnter(eventData: Event) {
+    console.log('mouseenter eventData : ', eventData);
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
   }
+
+  @HostListener('mouseleave') mouseLeave(eventDate: Event){
+    console.log('mouseleave Event data: ', eventDate);
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+  }
+
+
 
 }
