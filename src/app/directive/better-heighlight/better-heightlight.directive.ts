2dfbb374fc3655cb7cedbd5e5e3760e1faf16d27
Siddharth Jadhav <siddharth.jadhav2093@gmail.com>
2018-09-16 16:21:30 +0530

    property binding

--- src/app/directive/better-heighlight/better-heightlight.directive.ts
+++ src/app/directive/better-heighlight/better-heightlight.directive.ts
@@ -1,29 +1,33 @@
-import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHeightlight]'
 })
-export class BetterHeightlightDirective implements OnInit{
+export class BetterHeightlightDirective implements OnInit {
 
+  // tslint:disable-next-line:no-input-rename
+  @Input() defaultColor: string;
+  @Input() heighLightColor: string;
   // using this you can bind any property on that dom
-  @HostBinding('style.backgroundColor') backroundColor: string = 'transparent';
+  @HostBinding('style.backgroundColor') backroundColor: string;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    this.backroundColor = this.defaultColor;
   }
 
   @HostListener('mouseenter') mouseEnter(eventData: Event) {
     console.log('mouseenter eventData : ', eventData);
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-    this.backroundColor = 'red';
+    this.backroundColor = this.heighLightColor;
   }
 
-  @HostListener('mouseleave') mouseLeave(eventDate: Event){
+  @HostListener('mouseleave') mouseLeave(eventDate: Event) {
     console.log('mouseleave Event data: ', eventDate);
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
-    this.backroundColor = 'transparent';
+    this.backroundColor = this.defaultColor;
   }
 
 
